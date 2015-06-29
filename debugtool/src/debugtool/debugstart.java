b6912954c5c94763b1ff0368fc6255a424b08839
pradee515 <kalisetti@gmail.com>
2015-06-29 10:58:25 -0700

    Added the continual running of the program until a specific option is selected

--- debugtool/src/debugtool/debugstart.java
+++ debugtool/src/debugtool/debugstart.java
@@ -6,30 +6,42 @@ public class debugstart {
 	
 	public static void main(String[] args)
 	{
-		
-	 System.out.println("#################################################################");
-	 System.out.println("####################### DEBUG TOOL ##############################");
-	 System.out.println("#################################################################");
-	 System.out.println("PLEASE CHOOSE YOUR OPTIONS FROM BELOW");
-	 System.out.println("1) Your system properties    2) Your tcp connections information");
-	 Scanner sc = new Scanner(System.in);
-	 int k = sc.nextInt();
-       if(k==1)
-       {   systemProperties sp = new systemProperties();
-                   sp.printSystemProperties();
-    	    
-       }
-       
-       else if(k==2)
-        {  
-    	    networking np = new networking();
-    	       np.netstatRunner();
-        	 
-        }
-       else
-       {
-    	    System.out.println("No option is selected , so logging out");
-       }
-	}
+		     Boolean running = true;
+		      while(running)
+		        { 
+		    	         System.out.println(" ");
+						 System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+						 System.out.println("@@@@@@@@@@@@@@@@@@@ GENERAL DEBUG TOOL 1@@@@@@@@@@@@@@@@@@@@@@@@@@");
+						 System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+						 System.out.println(" ");
+						 System.out.println("PLEASE CHOOSE YOUR OPTIONS FROM BELOW");
+						 System.out.println("1) Your system properties    2) Your tcp connections information    3) EXIT "); 
+						 Scanner sc = new Scanner(System.in);
+						 int k = sc.nextInt();
+					       if(k==1)
+					       {   systemProperties sp = new systemProperties();
+					                   sp.printSystemProperties();
+					    	    
+					       }
+					       
+					       else if(k==2)
+					        {  
+					    	    networking np = new networking();
+					    	       np.netstatRunner();
+					        	 
+					        }
+					       else if(k ==3)
+					       {
+					    	    break;
+					       }
+					       else
+					       {
+					    	    System.out.println("No proper option is selected , going back");
+					    	   
+					       }
+					       
+					       System.out.println("############ PRESS CTRL+C ANYTIME TO END THE PROGRAM ###############");
+				   }
+	 }
 
 }
